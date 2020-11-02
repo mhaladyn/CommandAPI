e5d6b967b46c84531c92d9d8c717ee805d111309
michal <michal.haladyn@contechnet.de>
2020-11-02 16:12:39 +0100

    Initial Commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController: ControllerBase
+    {
+        [HttpGet]
+        [HttpPost]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            if(Request.Method.Equals("GET")){
+                return new string[]{ "HttpMethod", "GET", "this", "is", "hard", "coded"};
+            }
+            else{
+                return new string[]{ "HttpMethod", "POST", "this", "is", "hard", "coded"};
+            }
+        }
+    }
+}
