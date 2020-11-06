d37734a7836334b660c1301d6c93bbc73a516905
michal <michal.haladyn@contechnet.de>
2020-11-06 15:59:45 +0100

    Added Model and Mock Repository

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Data;
+using CommandAPI.Models;
 
 namespace CommandAPI.Controllers
 {
@@ -7,16 +9,42 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController: ControllerBase
     {
-        [HttpGet]
-        [HttpPost]
-        public ActionResult<IEnumerable<string>> Get()
+        private readonly ICommandAPIRepo _repository;
+
+        public CommandsController(ICommandAPIRepo repositry)
         {
-            if(Request.Method.Equals("GET")){
-                return new string[]{ "HttpMethod", "GET", "this", "is", "hard", "coded"};
-            }
-            else{
-                return new string[]{ "HttpMethod", "POST", "this", "is", "hard", "coded"};
-            }
+            _repository = repositry;
         }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if(commandItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(commandItem);
+        }
+
+
+        // [HttpGet]
+        // [HttpPost]
+        // public ActionResult<IEnumerable<string>> Get()
+        // {
+        //     if(Request.Method.Equals("GET")){
+        //         return new string[]{ "HttpMethod", "GET", "this", "is", "hard", "coded"};
+        //     }
+        //     else{
+        //         return new string[]{ "HttpMethod", "POST", "this", "is", "hard", "coded"};
+        //     }
+        // }
     }
 }
